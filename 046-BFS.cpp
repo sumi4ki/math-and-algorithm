b5ebbe33e116183bffd0ebea10f7b33b4f5b6bba
sumi4ki <153429945+sumi4ki@users.noreply.github.com>
2026-02-27 13:16:04 +0900

    improve 046 program

--- 046-BFS.cpp
+++ 046-BFS.cpp
@@ -6,77 +6,61 @@
 
 using namespace std;
 
-int H, W;
-
 struct Point {
     int y;
     int x;
-    int val;
+    Point() : y(0), x(0) {}
+    Point(int y, int x) : y(y), x(x) {}
 };
 
 int main() {
     // 入力
+    int H, W;
     cin >> H >> W;
-    vector<int> S(2), G(2);
-    cin >> S[0] >> S[1] >> G[0] >> G[1];
-    S[0]--; S[1]--; G[0]--; G[1]--;
+    Point S, G;
+    cin >> S.y >> S.x >> G.y >> G.x;
+    S.y--; S.x--; G.y--; G.x--;
 
     vector<string> maze(H);
     for(int i=0; i<H; i++) cin >> maze[i];
     
+    // 方向ベクトル
+    vector<int> dy{-1, 0, 1, 0}, dx{0, 1, 0, -1};
+
     // 初期化
-    vector<Point> dist;
+    vector<vector<int>> dist(H, vector<int>(W));
     for(int i=0; i<H; i++) {
         for(int j=0; j<W; j++) {
-            if(maze[i][j] == '.') {
-                dist.push_back(Point{i, j, -1});
-            }
+            dist[i][j] = -1;
         }
     }
     queue<Point> Q;
-    auto s = Point{S[0], S[1], 0};
-    Q.push(s);
-    
+    Q.push(S);
+    dist[S.y][S.x] = 0;
     // BFS
     while(!Q.empty()) {
         // 先頭要素を調べて
-        Point p_cur = Q.front();
+        auto cur = Q.front();
         // 取り出す
         Q.pop();
-        
-        // 現在位置
-        vector<int> cur{p_cur.y, p_cur.x};
         // 現在位置から4方向調べる
-        for(int dir=0; dir<4; dir++) {
-            vector<int> nex;
-            switch(dir) {
-                case 0: // up
-                    nex = {cur[0] - 1, cur[1]};
-                    break;
-                case 1: // right
-                    nex = {cur[0], cur[1] + 1};
-                    break;
-                case 2: // down
-                    nex = {cur[0] + 1, cur[1]};
-                    break;
-                case 3: // left
-                    nex = {cur[0], cur[1] - 1};
-            }
-            // 到達可能かつ未訪問であるか調べる
-            for(auto& d : dist) {
-                // dist に nex の座標があれば、必ず床である
-                if(nex[0] == d.y && nex[1] == d.x && d.val == -1) {
-                    d.val = p_cur.val + 1;
-                    Q.push(d);
+        for(int i=0; i<4; i++) {
+            int ny = cur.y + dy[i];
+            int nx = cur.x + dx[i];
+            
+            // 範囲内か、壁でないか、未訪問かをチェック
+            if(ny >= 0 && nx >= 0 && ny < H && nx < W) {
+                if(maze[ny][nx] != '#') {
+                    if(dist[ny][nx] == -1) {
+                        dist[ny][nx] = dist[cur.y][cur.x] + 1;
+                        Q.push(Point(ny, nx));
+                    }
                 }
             }
         }
     }
     
     // ゴール地点の dist が最小の手数になっている
-    for(const auto& d : dist) {
-        if(d.y == G[0] && d.x == G[1]) cout << d.val << endl; // endl がないと通らない
-        // cout << d.y << " " << d.x << " " << d.val << endl;
-    }
+    cout << dist[G.y][G.x];
     return 0;
 }
